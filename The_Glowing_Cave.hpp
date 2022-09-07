bff6b4a9dde88f7e635937e1feb2631a7db00df2
Mozz <mozzarellamonster@users.noreply.github.com>
2022-09-07 00:00:09 -0600

    Added new files for The Glowing Cave world.

---
+++ The_Glowing_Cave.hpp
@@ -0,0 +1,14 @@
+// Header file for The_Glowing_Cave.hpp
+#ifndef THE_GLOWING_CAVE_HPP
+#define THE_GLOWING_CAVE_HPP
+
+#include<string>
+
+extern bool explored_left_tunnel;
+extern bool explored_right_tunnel;
+extern bool explored_path;
+
+extern void the_glowing_cave_text();
+extern void the_glowing_cave();
+
+#endif
