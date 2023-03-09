495c63a735a5e2a8731aa9d3d9ad68901bbd5907
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-08 23:02:11 -0700

    Fixed minor mistake.

--- Trackers.cpp
+++ Trackers.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-string water_riddle = "\tUnhindered, I can destroy cities, yet tamed I supply them. I carve the very Earth, yet flesh does not yield.\n"
+string water_riddle = "\t\"Unhindered, I can destroy cities, yet tamed I supply them. I carve the very Earth, yet flesh does not yield.\n"
                       "\tI reign over the many kingdoms of man, for I have seen them all. For millennia you have tried to control me,\n"
                       "\tto confine me, yet you are the greatest prisoner, for you cannot live without me.\"";
 
-string wind_riddle = "\tI roam the world, wild and free. Sailors and storms both rely on me.\n"
+string wind_riddle = "\t\"I roam the world, wild and free. Sailors and storms both rely on me.\n"
                      "\tI can caress your skin and steal your breath away. Or I can tear your home apart and throw you away.\"";
 
-string fire_riddle = "\tA force to be reckoned with, it was I that gave man the power they possess. I am protection, I am light, but I have no loyalties.\n"
+string fire_riddle = "\t\"A force to be reckoned with, it was I that gave man the power they possess. I am protection, I am light, but I have no loyalties.\n"
                      "\tI devour fiercely, but have no thirst, for if I drink I will die.\"";
 
 string correct_riddle = "You walk up to the wall with the same symbol and gently touch it.\n"
