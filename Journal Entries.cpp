802acf2e6360de9aa3a04fc6ac1db39b0446c44e
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-15 23:38:29 -0600

    Minor text edits.

--- Journal Entries.cpp
+++ Journal Entries.cpp
@@ -4,19 +4,19 @@
 
 using namespace std;
 
-string journal_entry_1 = "\n\t6/7/01\n"
+string journal_entry_1 = "\n\t06/07/01\n"
                          "\tThe archeological dig has done tremendous work,\n"
                          "\tI really must stop by the excavation director's tent and commend him for his excellent service.\n\n"
                          "\tThe dig team has uncovered a relic most peculiar.\n"
                          "\tIt appears to be a carved stone ring inset with unusual hieroglyphic markings carved around the circumference of the ring.\n"
                          "\tI have taken it into my tent for further study.\n\n";
 
-string journal_entry_2 = "\n\t6/27/01\n"
+string journal_entry_2 = "\n\t06/27/01\n"
                          "\tI have received word back from Marilyn at the university. She has never seen symbols quite like those present on the mystery stone ring, either.\n"
                          "\tThe closest comparison we have is ancient Egyptian, but there's enough differences between the two to rule out any cultural mixing.\n"
                          "\tI will need to study this artifact more.\n\n";
 
-string journal_entry_3 = "\n\t7/14/05\n"
+string journal_entry_3 = "\n\t07/14/05\n"
                          "\tThe ring has responded to my actions. There is now a thinly opaque layer of liquid-like energy in the middle of the ring.\n"
                          "\tI will need to calibrate the machine even further. These old records are a massive pain to translate, but I'm definitely on to something here.\n"
                          "\tI will solve the mystery of this stone ring.\n\n";
@@ -30,7 +30,7 @@ string journal_entry_4 = "\n\t11/13/08\n"
                          "\tThe next few experiments will involve me sending some organic lifeforms through, I need to ensure safe passage.\n"
                          "\tI wish to see this temple with up close with my own eyes.\n\n";
 
-string journal_entry_5 = "\n\t7/08/09\n"
+string journal_entry_5 = "\n\t07/08/09\n"
                          "\tI've brought back more specimens from my trips through the Gates. I cannot help myself, I must study them.\n"
                          "\tMy curiosity for the worlds I've been to knows no bounds. I'm so intrigued by every new thing I've learned and discovered,\n"
                          "\tI have to study them more. I've acquired some organic growths that are incredibly similar to mold from our world, except that\n"
