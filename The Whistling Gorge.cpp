be7b987a48219905d4bc319c2341e55b60fe6337
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-20 21:26:13 -0600

    Added two new files for new passage.

---
+++ The Whistling Gorge.cpp
@@ -0,0 +1,10 @@
+// File containing functions for The Whistling Gorge passage
+#include<iostream>
+#include<string>
+#include"The_Whistling_Gorge.hpp"
+#include"Functions.hpp"
+#include"Trackers.hpp"
+#include"Items.hpp"
+
+using namespace std;
+
---
+++ The Whistling Gorge.hpp
@@ -0,0 +1,11 @@
+// Header file for The Whistling Gorge
+#ifndef THE_WHISTLING_GORGE_HPP
+#define THE_WHISTLING_GORGE_HPP
+
+#include<string>
+
+using namespace std;
+
+
+
+#endif
