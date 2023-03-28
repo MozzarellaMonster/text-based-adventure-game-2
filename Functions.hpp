ce61d339d4fdfe483e7c296147fa2ff30b18ec29
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-27 22:06:06 -0600

    Removed some functions and fixed several issues.

--- Functions.hpp
+++ Functions.hpp
@@ -13,7 +13,7 @@ extern void reset();
 extern void retry();
 extern void retry(void (*func)());
 extern void present_options(vector<string> option_names, vector<string> dialogue, void (*func)());
-extern void archway(string message, string name, bool archway_read, void (*func)());
+extern void archway(string message, string name, bool *archway_read, void (*func)());
 extern void deposit(string key);
 extern void determine_riddle();
 extern bool inv_contains(string object);
@@ -30,13 +30,5 @@ extern void temple_second_room();
 
 extern void the_desert_of_the_dead_text();
 extern void the_desert_of_the_dead();
-
-extern void the_heart_of_the_colony_text();
-extern void the_heart_of_the_colony();
-
-extern void the_garden();
-extern void the_whistling_gorge();
-extern void the_swamplands();
-extern void the_labyrinth();
 
 #endif
