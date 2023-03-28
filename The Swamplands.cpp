3d8ed4c1404ed0e7c35e7b172cd2aa9ec828f8c4
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-27 22:58:49 -0600

    Added files with basic layout for The Swamplands passage.

---
+++ The Swamplands.cpp
@@ -0,0 +1,20 @@
+// File containing functions for The Swamplands passage
+#include <iostream>
+#include <string>
+#include "The Swamplands.hpp"
+#include "Functions.hpp"
+#include "Journal Entries.hpp"
+#include "Items.hpp"
+#include "Trackers.hpp"
+
+using namespace std;
+
+void the_swamplands_text()
+{
+
+}
+
+void the_swamplands()
+{
+    
+}
---
+++ The Swamplands.hpp
@@ -0,0 +1,12 @@
+// Header file for The Swamplands.cpp
+#ifndef THE_SWAMPLANDS_HPP
+#define THE_SWAMPLANDS_HPP
+
+#include <string>
+
+using namespace std;
+
+extern void the_swamplands_text();
+extern void the_swamplands();
+
+#endif
