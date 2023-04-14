16a9b882262f32b4667447a5e33c9cce646c23c9
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-04-14 00:58:22 -0600

    Added more story and completed one available path.

--- The Oasis.cpp
+++ The Oasis.cpp
@@ -32,11 +32,31 @@ void the_oasis()
 
     if(choice == 'A' || choice == 'a')
     {
-        cout << "You decide to explore the left path.";
+        cout << "You decide to explore the left path.\n"
+                "You look towards the left path. It lays in darkness, an overarching tunnel of branches that goes deeper into the jungle. You hesitate for just a moment, but step forward into the tunnel.\n"
+                "After several minutes, you find yourself in a small clearing. The floor of the clearing is littered with strange-looking twigs. Curious, you lean down and pick up a few of the twigs, rolling them between your fingers.\n"
+                "You inspect one of the twigs closely, small, white and perforated. Very brittle as well, you note as it snaps easily between your fingers. It is then you notice the marrow in the center of the twig.\n"
+                "These aren't twigs, they're bones. Your blood freezes as you drop the bones you're holding and turn to run. As you turn, a low growl rumbles all around you, and steadily grows louder.\n"
+                "You quickly turn to look at the entrance to the clearing, but you're met with a close-knit, intertwined collection of branches blocking the entrance. You're trapped here. The rumble is incredibly loud now.\n"
+                "It is then you hear wooden creaks as several branches are shifted away from the 'wall' of the clearing opposite the entrance, and a hideous gaping maw is shown in the space left behind.\n"
+                "Your heart gallops in your chest as the humid breath of the huge mouth hits you and several large tentacle-like tongues slowly wind their way down the gullet of the thing in the branches.\n"
+                "You desperately turn back to the entrance of the clearing and tear at the branches blocking your way, slivers and cuts be damned. You snap away as many of the branches as you can, but see through\n"
+                "the spaces between them that they are quickly replaced with more as the entrance is further barricaded, cutting off the dim light from the tunnel in the process and hopelessly sealing you in.\n"
+                "The tongues slither ever closer, taking their time to savor the moment - playing with their prey. You feel the breath of the thing grow in anticipation as one of the tongues finally wrap around your ankle,\n"
+                "another around your waist. As a final futile attempt at escape, you plunge your arm into the interwoven barricade of branches and grab one of them. Slowly, the tongue grows taut as the thing struggles to pull you in.\n"
+                "It pulls and pulls, but you hold fast, pulling yourself away whenever the thing grows tired. Finally impatient with this tug-of-war, the thing's final tongue wraps around the shoulder of the arm planted in the\n"
+                "web of branches and with great force, pulls. You hear a sickening pop as your shoulder dislocates and you scream in pain, but the thing is not done. It pulls even harder and harder, the fabric of your clothes\n"
+                "tearing at the seams. The pain is unbearable. And with a final tug, the things pulls you free of the branches, several pieces of skin left behind on sharp thorns within. Tears flow freely now as your limp arm\n"
+                "drags behind you and you close your eyes in defeat as you enter the warm mouth of the thing.\n";
+        
+        cout << "\n\nEnding 14: Eaten\n";
+        print_line();
+        retry(&the_oasis);
     }
     else if(choice == 'B' || choice == 'b')
     {
-        cout << "You decide to explore the middle path.";
+        cout << "You decide to explore the middle path.\n"
+                "Peering through the trees, you can just make out a beautiful waterfall in the distance, a small rainbow reflected in the spray.\n";
     }
     else if(choice == 'C' || choice == 'c')
     {
