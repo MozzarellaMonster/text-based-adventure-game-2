5967488e5e3846b4117354454f47e09e22e2c71e
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-04-19 23:51:08 -0600

    Added final path.

--- The Oasis.cpp
+++ The Oasis.cpp
@@ -74,7 +74,23 @@ void the_oasis()
     else if(choice == 'C' || choice == 'c')
     {
         cout << "You decide to explore the right path.\n"
-                "";
+                "You go through the trees until you reach the foot of a hill. After a brief climb, you find yourself at the top of an enormous cliff looking out over a misty valley with miles and miles of jungle\n"
+                "all around. Stricken by the beauty of it all, you approach the edge of the cliff and slowly, carefully get to your knees and look over the edge of the cliff. Looking down, you can see a small waterfall\n"
+                "some meters down coming out of the side of the cliff. You slowly start to bring yourself back up when you notice something in your peripheral vision. You snap your head in the direction of the large moving\n"
+                "shadow and see a giant condor diving toward you with its massive claws outstretched. You clumsily turn and try to dive back towards the jungle trees, but are caught up in the huge talons of the massive bird\n"
+                "mid-dive. Terrified, you strike at feet of the beast, trying desperately to stab at it with the dagger from the temple. You stab and stab, but to no avail, the scutum of the massive bird is just too thick.\n"
+                "With one final desperate try, you bring down the dagger hard, but the bird suddenly dives and you drop the dagger. You watch as it falls into the jungle below. You stare as the trees past below you blindingly\n"
+                "fast, but soon the bird slows its descent and unceremoniously drops you into a huge collection of branches and dried leaves. You slowly get back up as the bird caws once and leaves. You grab your shoulder,\n"
+                "gently pressing on it. Pain flares as you try to move it, the bird must have dislocated it when it dropped you. As you catch your breath, something moves in the shadows to your right. Your quickly look in that\n"
+                "direction and see a fuzzy shape move in the darkness. Startled, you turn to face it and slowly start to back away. You hear some muffled crooning sounds before the fuzzy shapes rights itself and steps into the light.\n"
+                "It's a giant baby condor, still flightless and looking right at you with hungry intent. You move slowly away from it, try to look for an exit, well aware of being the young bird's intended dinner. The baby understands\n"
+                "this too, and suddenly charges at you. With speed you didn't know you had, you deftly dive out of the way, but land painfully on some branches. Gritting your teeth against the pain, you start to stand up, but the\n"
+                "baby is faster and knocks you into the wall of the nest. Pain flares in your ribs as you hear a sickening crack and you scream. The baby condor wastes no time and bites down on your midsection, hard. Your abdomen crunches\n"
+                "and is torn open as the baby condor pulls away and eats its meal. You only have a moment to process your death as your vision fades and you hear the baby condor crooning before coming back in for another bite.\n";
+        
+        cout << "\n\nEnding 16: Baby Food\n";
+        print_line();
+        retry(&the_oasis);
     }
     else
     {
