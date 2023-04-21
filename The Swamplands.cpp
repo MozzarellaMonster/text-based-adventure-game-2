3d782d84260ec48923054cd5662ce7107c3cbb9a
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-04-21 00:01:29 -0600

    Added some story and basic function structure.

--- The Swamplands.cpp
+++ The Swamplands.cpp
@@ -9,12 +9,22 @@
 
 using namespace std;
 
+// Possible scenarios - alligators, small 'swamp spirits' that hunt you down and turn you into a shrunken head, a strange being stalking you possessing the Black Heart, 'living hive', cannot stay here past dusk
+
 void the_swamplands_text()
 {
-
+    cout << "You stumble out of the Gate and immediately get your foot stuck in a deep puddle of mud. The mud sucks at your shoes as you desperately try to free your foot without falling over.\n"
+            "Thankfully, after much wiggling and pulling, you manage to free your foot, shoe and all, from the mud without so much as a slight stumble. You finally look up from the muddy puddle\n"
+            "and take in your surroundings - you're in an unfamiliar swamp. Tall trees with large roots dot the landscape and tall grass covers the few areas that are above-water.\n"
+            "Taking a cautious step forward, you stay on what little solid ground you can find, until you find a large boulder sticking out of the water that allows you some height to take in more of your surroundings.\n"
+            "Standing atop it, you see that the swamp is divided into three distinct areas - one shady area covered in weeping willow trees consisting mainly of small islands, another area with a considerable\n"
+            "lack of large trees mostly consisting of tall grass and waist-high water, and the final area making up the majority of the swamp with large trees and very deep water you would need a boat to go through.\n"
+            "Thankfully, from your high position, you spot a dock with a small boat anchored to it. Climbing down, you make your way to the dock where you find a barrel of rope with a piece of folded paper nested within.\n"
+            "Unfolding the piece of paper, you read:\n\n";
+    cout << ;
 }
 
 void the_swamplands()
 {
-    
+    cout << "";
 }
