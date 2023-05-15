eef03f2383ff600ad52971536db5ee007c1f360b
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-05-15 03:55:44 -0600

    Edited for code consistency.

--- The Oasis.cpp
+++ The Oasis.cpp
@@ -34,72 +34,74 @@ void the_oasis()
     cout << "C. Explore the right path\n";
     print_line();
     cin >> choice;
+    choice = tolower(choice);
 
-    if(choice == 'A' || choice == 'a')
+    switch(choice)
     {
-        cout << "You decide to explore the left path.\n"
-                "You look towards the left path. It lays in darkness, an overarching tunnel of branches that goes deeper into the jungle. You hesitate for just a moment, but step forward into the tunnel.\n"
-                "After several minutes, you find yourself in a small clearing. The floor of the clearing is littered with strange-looking twigs. Curious, you lean down and pick up a few of the twigs, rolling them between your fingers.\n"
-                "You inspect one of the twigs closely, small, white and perforated. Very brittle as well, you note as it snaps easily between your fingers. It is then you notice the marrow in the center of the twig.\n"
-                "These aren't twigs, they're bones. Your blood freezes as you drop the bones you're holding and turn to run. As you turn, a low growl rumbles all around you, and steadily grows louder.\n"
-                "You quickly turn to look at the entrance to the clearing, but you're met with a close-knit, intertwined collection of branches blocking the entrance. You're trapped here. The rumble is incredibly loud now.\n"
-                "It is then you hear wooden creaks as several branches are shifted away from the 'wall' of the clearing opposite the entrance, and a hideous gaping maw is shown in the space left behind.\n"
-                "Your heart gallops in your chest as the humid breath of the huge mouth hits you and several large tentacle-like tongues slowly wind their way up through the gullet of the thing in the branches.\n"
-                "You desperately turn back to the entrance of the clearing and tear at the branches blocking your way, slivers and cuts be damned. Remembering the dagger, you bring it out and start cutting at the branches\n"
-                "only for you to clumsily drop it in the nest of bones. Stooping to search, you see the tongues move closer and decide there's no time as you stand back up and resume your escape attempt.\n"
-                "You snap away as many of the branches as you can, but see through the spaces between them that they are quickly replaced with more as the entrance is further barricaded,\n"
-                "cutting off the dim light from the tunnel in the process and hopelessly sealing you in. The tongues slither ever closer, taking their time to savor the moment - playing with their prey.\n"
-                "You feel the breath of the thing grow in anticipation as one of the tongues finally wraps itself around your ankle, another around your waist. As a final futile attempt at escape,\n"
-                "you plunge your arm into the interwoven barricade of branches and grab one of them. Slowly, the tongue grows taut as the thing struggles to pull you in.\n"
-                "It pulls and pulls, but you hold fast, pulling yourself away whenever the thing grows tired. Finally impatient with this tug-of-war, the thing's final tongue wraps around the shoulder of the arm planted in the\n"
-                "web of branches and with great force, pulls. You hear a sickening pop as your shoulder dislocates and you scream in pain, but the thing is not done. It pulls even harder and harder, the fabric of your clothes\n"
-                "tearing at the seams. The pain is unbearable. And with a final tug, the things pulls you free of the branches as you lose your grip, several pieces of skin left behind on sharp thorns within.\n"
-                "Tears flow freely now as your limp arm drags behind you and you close your eyes in defeat as you enter the warm mouth of the thing.\n";
-        
-        cout << "\n\nEnding 14: Eaten\n";
-        print_line();
-        retry(&the_oasis);
-    }
-    else if(choice == 'B' || choice == 'b')
-    {   
-        if(!middle_path_intro)
-        {
-            cout << "You decide to explore the middle path.\n"
-                    "Peering through the trees, you can just make out a beautiful waterfall in the distance, a small rainbow reflected in the spray.\n"
-                    "You follow the path through the jungle until you stand on the shore of the small pond the waterfall pours into. You stare at the beauty all around you. \n"
-                    "The roar of the waterfall provides some natural white noise as you slowly close your eyes and simply soak in the moment. It's a nice reprieve from what you've been through lately.\n"
-                    "Several moments later, after taking one final deep breath, you finally open your eyes and return to your previous mindset: looking for what you need.\n";
-            middle_path_intro = true;
-        }
-        cout << "You stand on the shoreline of the small pond the roaring waterfall pours into. To your left, you see an unusually neatly stacked pile of rocks set against a cliff wall next to the waterfall,\n"
-                "to your left lies a log overgrown with mushrooms and moss, but appears hollow inside, and directly ahead of you lies the waterfall.\n";
-        pond_options();
-    }
-    else if(choice == 'C' || choice == 'c')
-    {
-        cout << "You decide to explore the right path.\n"
-                "You go through the trees until you reach the foot of a hill. After a brief climb, you find yourself at the top of an enormous cliff looking out over a misty valley with miles and miles of jungle\n"
-                "all around. Stricken by the beauty of it all, you approach the edge of the cliff and slowly, carefully get to your knees and look over the edge of the cliff. Looking down, you can see a small waterfall\n"
-                "some meters down coming out of the side of the cliff. You slowly start to bring yourself back up when you notice something in your peripheral vision. You snap your head in the direction of the large moving\n"
-                "shadow and see a giant condor diving toward you with its massive claws outstretched. You clumsily turn and try to dive back towards the jungle trees, but are caught up in the huge talons of the massive bird\n"
-                "mid-dive. Terrified, you strike at feet of the beast, trying desperately to stab at it with the dagger from the temple. You stab and stab, but to no avail, the scutum of the massive bird is just too thick.\n"
-                "With one final desperate try, you bring down the dagger hard, but the bird suddenly dives and you drop the dagger. You watch as it falls into the jungle below. You stare as the trees past below you blindingly\n"
-                "fast, but soon the bird slows its descent and unceremoniously drops you into a huge collection of branches and dried leaves. You slowly get back up as the bird caws once and leaves. You grab your shoulder,\n"
-                "gently pressing on it. Pain flares as you try to move it, the bird must have dislocated it when it dropped you. As you catch your breath, something moves in the shadows to your right. Your quickly look in that\n"
-                "direction and see a fuzzy shape move in the darkness. Startled, you turn to face it and slowly start to back away. You hear some muffled crooning sounds before the fuzzy shapes rights itself and steps into the light.\n"
-                "It's a giant baby condor, still flightless and looking right at you with hungry intent. You move slowly away from it, try to look for an exit, well aware of being the young bird's intended dinner. The baby understands\n"
-                "this too, and suddenly charges at you. With speed you didn't know you had, you deftly dive out of the way, but land painfully on some branches. Gritting your teeth against the pain, you start to stand up, but the\n"
-                "baby is faster and knocks you into the wall of the nest. Pain flares in your ribs as you hear a sickening crack and you scream. The baby condor wastes no time and bites down on your midsection, hard. Your abdomen crunches\n"
-                "and is torn open as the baby condor pulls away and eats its meal. You only have a moment to process your death as your vision fades and you hear the baby condor crooning before coming back in for another bite.\n";
-        
-        cout << "\n\nEnding 16: Baby Food\n";
-        print_line();
-        retry(&the_oasis);
-    }
-    else
-    {
-        try_again();
-        the_oasis();
+        case 'a':
+            cout << "You decide to explore the left path.\n"
+                    "You look towards the left path. It lays in darkness, an overarching tunnel of branches that goes deeper into the jungle. You hesitate for just a moment, but step forward into the tunnel.\n"
+                    "After several minutes, you find yourself in a small clearing. The floor of the clearing is littered with strange-looking twigs. Curious, you lean down and pick up a few of the twigs, rolling them between your fingers.\n"
+                    "You inspect one of the twigs closely, small, white and perforated. Very brittle as well, you note as it snaps easily between your fingers. It is then you notice the marrow in the center of the twig.\n"
+                    "These aren't twigs, they're bones. Your blood freezes as you drop the bones you're holding and turn to run. As you turn, a low growl rumbles all around you, and steadily grows louder.\n"
+                    "You quickly turn to look at the entrance to the clearing, but you're met with a close-knit, intertwined collection of branches blocking the entrance. You're trapped here. The rumble is incredibly loud now.\n"
+                    "It is then you hear wooden creaks as several branches are shifted away from the 'wall' of the clearing opposite the entrance, and a hideous gaping maw is shown in the space left behind.\n"
+                    "Your heart gallops in your chest as the humid breath of the huge mouth hits you and several large tentacle-like tongues slowly wind their way up through the gullet of the thing in the branches.\n"
+                    "You desperately turn back to the entrance of the clearing and tear at the branches blocking your way, slivers and cuts be damned. Remembering the dagger, you bring it out and start cutting at the branches\n"
+                    "only for you to clumsily drop it in the nest of bones. Stooping to search, you see the tongues move closer and decide there's no time as you stand back up and resume your escape attempt.\n"
+                    "You snap away as many of the branches as you can, but see through the spaces between them that they are quickly replaced with more as the entrance is further barricaded,\n"
+                    "cutting off the dim light from the tunnel in the process and hopelessly sealing you in. The tongues slither ever closer, taking their time to savor the moment - playing with their prey.\n"
+                    "You feel the breath of the thing grow in anticipation as one of the tongues finally wraps itself around your ankle, another around your waist. As a final futile attempt at escape,\n"
+                    "you plunge your arm into the interwoven barricade of branches and grab one of them. Slowly, the tongue grows taut as the thing struggles to pull you in.\n"
+                    "It pulls and pulls, but you hold fast, pulling yourself away whenever the thing grows tired. Finally impatient with this tug-of-war, the thing's final tongue wraps around the shoulder of the arm planted in the\n"
+                    "web of branches and with great force, pulls. You hear a sickening pop as your shoulder dislocates and you scream in pain, but the thing is not done. It pulls even harder and harder, the fabric of your clothes\n"
+                    "tearing at the seams. The pain is unbearable. And with a final tug, the things pulls you free of the branches as you lose your grip, several pieces of skin left behind on sharp thorns within.\n"
+                    "Tears flow freely now as your limp arm drags behind you and you close your eyes in defeat as you enter the warm mouth of the thing.\n";
+            
+            cout << "\n\nEnding 14: Eaten\n";
+            print_line();
+            retry(&the_oasis);
+            break;
+            
+        case 'b':
+            if(!middle_path_intro)
+            {
+                cout << "You decide to explore the middle path.\n"
+                        "Peering through the trees, you can just make out a beautiful waterfall in the distance, a small rainbow reflected in the spray.\n"
+                        "You follow the path through the jungle until you stand on the shore of the small pond the waterfall pours into. You stare at the beauty all around you. \n"
+                        "The roar of the waterfall provides some natural white noise as you slowly close your eyes and simply soak in the moment. It's a nice reprieve from what you've been through lately.\n"
+                        "Several moments later, after taking one final deep breath, you finally open your eyes and return to your previous mindset: looking for what you need.\n";
+                middle_path_intro = true;
+            }
+            cout << "You stand on the shoreline of the small pond the roaring waterfall pours into. To your left, you see an unusually neatly stacked pile of rocks set against a cliff wall next to the waterfall,\n"
+                    "to your left lies a log overgrown with mushrooms and moss, but appears hollow inside, and directly ahead of you lies the waterfall.\n";
+            pond_options();
+            break;
+
+        case 'c':
+            cout << "You decide to explore the right path.\n"
+                    "You go through the trees until you reach the foot of a hill. After a brief climb, you find yourself at the top of an enormous cliff looking out over a misty valley with miles and miles of jungle\n"
+                    "all around. Stricken by the beauty of it all, you approach the edge of the cliff and slowly, carefully get to your knees and look over the edge of the cliff. Looking down, you can see a small waterfall\n"
+                    "some meters down coming out of the side of the cliff. You slowly start to bring yourself back up when you notice something in your peripheral vision. You snap your head in the direction of the large moving\n"
+                    "shadow and see a giant condor diving toward you with its massive claws outstretched. You clumsily turn and try to dive back towards the jungle trees, but are caught up in the huge talons of the massive bird\n"
+                    "mid-dive. Terrified, you strike at feet of the beast, trying desperately to stab at it with the dagger from the temple. You stab and stab, but to no avail, the scutum of the massive bird is just too thick.\n"
+                    "With one final desperate try, you bring down the dagger hard, but the bird suddenly dives and you drop the dagger. You watch as it falls into the jungle below. You stare as the trees past below you blindingly\n"
+                    "fast, but soon the bird slows its descent and unceremoniously drops you into a huge collection of branches and dried leaves. You slowly get back up as the bird caws once and leaves. You grab your shoulder,\n"
+                    "gently pressing on it. Pain flares as you try to move it, the bird must have dislocated it when it dropped you. As you catch your breath, something moves in the shadows to your right. Your quickly look in that\n"
+                    "direction and see a fuzzy shape move in the darkness. Startled, you turn to face it and slowly start to back away. You hear some muffled crooning sounds before the fuzzy shapes rights itself and steps into the light.\n"
+                    "It's a giant baby condor, still flightless and looking right at you with hungry intent. You move slowly away from it, try to look for an exit, well aware of being the young bird's intended dinner. The baby understands\n"
+                    "this too, and suddenly charges at you. With speed you didn't know you had, you deftly dive out of the way, but land painfully on some branches. Gritting your teeth against the pain, you start to stand up, but the\n"
+                    "baby is faster and knocks you into the wall of the nest. Pain flares in your ribs as you hear a sickening crack and you scream. The baby condor wastes no time and bites down on your midsection, hard. Your abdomen crunches\n"
+                    "and is torn open as the baby condor pulls away and eats its meal. You only have a moment to process your death as your vision fades and you hear the baby condor crooning before coming back in for another bite.\n";
+            
+            cout << "\n\nEnding 16: Baby Food\n";
+            print_line();
+            retry(&the_oasis);
+            break;
+
+        default:
+            try_again();
+            the_oasis();
     }
 }
 
