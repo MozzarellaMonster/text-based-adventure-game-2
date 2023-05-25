a09f6fc7f54187e19dbb49794c9f6aca8544e9b2
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-05-24 23:29:16 -0600

    Added new files.

---
+++ The Labyrinth.cpp
@@ -0,0 +1,19 @@
+// File containing functions for The Labyrinth passage
+#include <iostream>
+#include <string>
+#include "Functions.hpp"
+#include "Journal Entries.hpp"
+#include "Trackers.hpp"
+#include "Items.hpp"
+
+using namespace std;
+
+void the_labyrinth_text()
+{
+    
+}
+
+void the_labyrinth()
+{
+
+}
---
+++ The Labyrinth.hpp
@@ -0,0 +1,12 @@
+// Header file for The Labyrinth passage
+#ifndef THE_LABYRINTH_HPP
+#define THE_LABYRINTH_HPP
+
+#include <string>
+
+using namespace std;
+
+extern void the_labyrinth_text();
+extern void the_labyrinth();
+
+#endif
