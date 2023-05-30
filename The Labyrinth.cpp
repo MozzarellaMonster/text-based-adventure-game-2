36cc553bc1c826f126218a49ac816270e30ebe3e
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-05-30 01:13:26 -0600

    Finished intro text function.

--- The Labyrinth.cpp
+++ The Labyrinth.cpp
@@ -10,10 +10,20 @@ using namespace std;
 
 void the_labyrinth_text()
 {
-    cout << "";
+    cout << "Unlike the other portals, you don't seem to be forced through. Rather, you find the ingress rather to be much easier.\n"
+            "You look around you, at first it seems as though there is nothing but a long stone floor all around you polished to a mirror-like sheen.\n"
+            "Then, the blocky outlines of edges form around you in a bright white light, like a solid wire mesh in a 3D animating program.\n"
+            "Gradually, the meshes fill in with solid matter - more of the smooth, polished stone fills in the wire meshes, conforming to the shapes of them.\n"
+            "And soon, massive walls materialize right before your eyes as you stand at the beginning of a giant maze. As the surfaces fill in around you,\n"
+            "you finally take a step forward and truly take in your surroundings. Not only have walls sprang up on either side of you, but even the ceiling\n"
+            "of the massive chamber has wall formed in the pattern of a maze. To your amazement, the chamber doesn't seem to have any hard edges,\n"
+            "as you can see the floor slowly curve to meet the walls, and later, the ceiling. The sight dizzies you for a bit, and you lean against the wall nearby\n"
+            "as you gather your senses. Eventually, you stand back upright and move forward. You've come this far already, nothing's going to stop you now.\n";
+    the_labyrinth();
 }
 
 void the_labyrinth()
 {
+    char choice;
     cout << "";
 }
