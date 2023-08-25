39fc93b28d57b4bdfb25bdae74654980a9bd6710
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-08-25 02:46:27 -0600

    Fixed several bugs and made some minor corrections.

--- The_Glowing_Cave.cpp
+++ The_Glowing_Cave.cpp
@@ -30,11 +30,11 @@ void the_glowing_cave_text()
             "It's about as big as a tree, with a soft turquoise light emanating from the gills and cap of the mushroom.\n"
             "Trails of the soft light are made where your fingers brush the stem before softly fading like a heat-sensitive coating.\n"
             "After a moment, you finally move your eyes away from the mesmerizing mushroom and scan your surroundings.\n"
-            "You appear to be in a large cavern, surrounded by clusters of the strange glowing fungi scattered about the walls an floors of the cave.\n"
+            "You appear to be in a large cavern, surrounded by clusters of the strange glowing fungi scattered about the walls and floors of the cave.\n"
             "With the mushrooms providing the only light in the otherwise dark and dank cave, you slowly take a step forward.\n"
             "You accidentally step into a deep puddle you were trying to avoid and end up making a large splash sound that echoes a bit within the confines of the cave.\n"
             "You suddenly hear an odd shuffling noise. Unsettled, you slowly look up and can barely make out large shapes spread out across the ceiling of the underground chamber.\n"
-            "Bats. Very large bats. You stand stock still, your shoe now completely soaked, but you dare not wake the flying mammals."
+            "Bats. Very large bats. You stand stock still, your shoe now completely soaked, but you dare not wake the flying mammals.\n"
             "After the bat settles down, you quietly sigh in relief. Looking back toward the ground level of the cave, you see two tunnels branching off from the chamber you're in directly ahead of you.\n"
             "There is also another path on the opposite side of the wall, between some large mushrooms you see a well-tread path leading further into the cave.\n\n";
     
@@ -69,7 +69,8 @@ void the_glowing_cave()
     print_line();
     char choice = getch();
     choice = tolower(choice);
-
+    cout << choice;
+    
     print_line();
     switch(choice)
     {
@@ -90,7 +91,7 @@ void the_glowing_cave()
                         "A muffled roaring noise accompanies the slight mist spraying from the entrance, giving you the image of rushing water. The mist gently dampens your face as if to verify your thoughts.\n"
                         "You take a cautious step into the tunnel.\n";
                 
-                gc_left_tunnel();
+                gc_left_tunnel_text();
             }
             break;
             
@@ -108,10 +109,10 @@ void the_glowing_cave()
             else
             {
                 cout << "You decide to explore the right tunnel.\n"
-                        "You walk up to the right tunnel, noticing that this tunnel is darker in comparison to the other options, but not enough to keep you from seeing.\n"
+                        "You walk up to the right tunnel, noticing that this tunnel is darker in comparison to the others, but not enough to keep you from seeing.\n"
                         "The air here feels drier as well. In fact, it seems that the air from the chamber you're in is flowing out into the tunnel. Despite the fantastical and warm atmosphere of the cave so far,\n"
                         "you feel a sudden surge of trepidation staring into the dim tunnel, as if you were staring down the throat of a large leviathan about to swallow you whole.\n"
-                        "Closing your eyes and taking a deep breath, you brace yourself and grip the handle of the dagger, the cold metal bringing you some degree of comfort.\n"
+                        "Closing your eyes and taking a deep breath, you brace yourself and grip the handle of the dagger, the cold metal bringing you some sense of security.\n"
                         "After a while, you open your eyes and make your way down the tunnel.\n";
                 gc_right_tunnel();
             }
@@ -136,9 +137,8 @@ void the_glowing_cave()
             {
                 cout << "You decide to explore the well-tread path on the opposite side of the cavern.\n"
                         "You turn your back to the two tunnels and quietly make your way across the cave, careful not to make too much noise.\n"
-                        "You approach the well-tread path stretching between the fluorescent fungi, smaller mushrooms growing on either side of the pathway, giving the whole thing a comforting feeling of familiarity,\n"
-                        "despite the fact that this whole thing is entirely new to you. A small smile plays across your lips as you confidently walk down the path that so many others have tread upon before you.\n"
-                        "Following in the footprints of the Worldwalkers.\n";
+                        "You approach the well-tread path stretching between the fluorescent fungi, smaller mushrooms growing on either side of the pathway, giving the whole thing a comforting feeling of familiarity.\n"
+                        "A small smile plays across your lips as you confidently walk down the path that so many others have tread upon before you. Following in the footprints of the Worldwalkers.\n";
                 gc_pathway_text();
             }
             break;
@@ -238,24 +238,25 @@ void gc_right_tunnel()
         cout << "You go through the dry tunnel. The air flowing past your body as you head further in.\n"
                 "Soon, you come across a much larger cave, dim light pouring in through a crevice in the ceiling of the chamber.\n"
                 "You look up and instantly freeze, even more bats are present in this cave than the one you were just in.\n"
-                "Thankfully, these bats seem to be deep asleep, with very little movements coming from the furry horde.\n"
+                "Thankfully, these bats seem to be deep asleep, with very little movement coming from the furry horde.\n"
                 "You adjust your movements accordingly, ensuring that you don't make any loud noises. You carefully make your way across the chamber to the wall opposite the entrance.\n"
                 "On this wall are numerous cave paintings, distinct from the usual style of the Worldwalkers. From what you can make out, it depicts an intelligent humanoid species\n"
                 "encountering what appears to be the Worldwalkers coming from the cave. The paintings continue on to show that an agreement was struck to barter and trade between the two groups.\n"
-                "Some time passes before something affects the agreement, the Worldwalkers appear less in the paintings until they no longer show at all.\n"
+                "Some time passes before something affects the agreement and the Worldwalkers appear less in the paintings until they no longer show at all.\n"
                 "The rest of the paintings are cut off by large boulders blocking what appears to be the exit to the outside world.\n"
-                "Something caused the native humanoids to want to cut off contact with the Worldwalkers. And something caused the Worldwalkers to stop trade with the native humanoids.\n"
-                "It is then you find an interesting assortment of rocks. The rocks are stacked in a peculiar way that suggests they served as an altar of sorts. Unfortunately, most of the\n"
-                "altar was smashed in whatever caused the avalanche that blocked off the entrance. Most of what appear to be valuable artifacts are either shattered or broken in some other way.\n"
+                "Something caused the native humanoids to want to cut off contact with the Worldwalkers. And something caused the Worldwalkers to stop trade with the natives.\n"
+                "It is then you find an interesting assortment of rocks. The rocks are stacked in a peculiar way that suggests they served as an altar of sorts.\n"
+                "Unfortunately, most of the altar was smashed in whatever caused the avalanche that blocked off the entrance.\n"
+                "Most of what appear to be valuable artifacts are either shattered or broken in some other way.\n"
                 "Looking around, you find a strange-looking fossil that appears to have fallen off the altar, pieces of a smashed vase laying around it. You pick it up.\n"
-                "Immediately, your ears pop and you find that your hearing has become extremely acute. In fact, you can even hear the quiet snoring of the bats hanging above you.\n"
+                "Immediately, your ears pop and you find that your hearing has become extremely acute. In fact, you can even hear the quiet snoring of the bats hanging far above you.\n"
                 "The effect is so sudden, you end up dropping the fossil. It clatters loudly on the cave floor and the effect disappears. You quickly snatch it up and your acute hearing returns.\n"
                 "It is then you notice something has changed - you can't hear the snoring of the bats anymore.\n"
                 "Hesitantly, you stiffly lean your head up towards the ceiling of the cave. What you see makes your stomach drop - you see at least a hundred beady black eyes staring down at you.\n"
-                "You pause, and for a moment, nothing happens. Then, a drop of water falls off a nearby stalactite and like a starting pistol, you take off in a mad sprint toward the tunnel.\n"
-                "Immediately, the bats take to the air in a mammalian cyclone, screeching and diving towards you. Strangely, you can hear them coming towards you so well it allows you to dodge them before\n"
-                "they can attack you. In fact, with the addition of the screeching, it feels as though you can sense the entirety of the cave around you. You move around in it, dodging the bats as if\n"
-                "it was the most well-lit gym in the world and you were a star dodgeball player.\n"
+                "You pause, and for a moment, nothing happens. Then, a drop of water falls off a nearby stalactite and when it hits the ground, the sound echoes throughout your skull.\n"
+                "Like a starting pistol, you take off in a mad sprint toward the tunnel. Immediately, the bats take to the air in a mammalian cyclone, screeching and diving towards you.\n"
+                "Strangely, you can hear them coming towards you so well it allows you to dodge them before they can attack you.\n"
+                "In fact, it feels as though you can sense the entirety of the cave around you. You move around in it, dodging the bats as if it was the most well-lit gym in the world and you were a star athlete.\n"
                 "You make it to the tunnel and run through, an angry bat trying to claw its way in, but can't due to its size.\n"
                 "You smile, your newfound power helping you through the darkness, and you soon burst back out into the previous area. You look up towards the ceiling, but there are no bats to be found.\n\n";
         gc_found_fossil = true;
